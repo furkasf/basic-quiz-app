1880d5d0ee08ca3350062a6a733ba185724cc5c4
furkasf <60402673+furkasf@users.noreply.github.com>
2020-07-03 18:19:50 +0300

    Add files via upload

---
+++ jsQuizApp/js/app.js
@@ -0,0 +1,31 @@
+function submitAnswers(){
+	var total = 5;
+	var score = 0;
+	
+	// Get User Input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	
+	// Validation
+	for(i = 1; i <= total;i++){
+		(eval('q'+i) == null || eval('q'+i) == '')?alert('You missed question '+ i): false;
+	}
+	
+	// Set Correct Answers
+	var answers = ["b","a","d","b","d"];
+	
+	// Check Answers
+	for(i = 1; i <= total;i++){
+		(eval('q'+i) == answers[i - 1])?score++:null;
+	}
+	
+	// Display Results
+	var results = document.getElementById('results');
+	results.textContent = 'You scored '+score+'out of '+total;
+	alert('You scored '+score+' out of ' +total);
+	
+	return false;
+}
